5a63d90acf07de08cfcb2df91b14c43a103189a8
Belal Hamdeh <122100487+Hamdeh-Belal@users.noreply.github.com>
2025-04-14 21:43:20 +0300

    First update

--- src/utils/generateSummary.js
+++ src/utils/generateSummary.js
@@ -5,26 +5,31 @@ export async function generateSummary(bookTitle) {
   const prompt = `Summarize the book titled "${bookTitle}" in 3-5 sentences in simple English.`;
 
   try {
-    // const res = await axios.post(
-    //   "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
-    //   {
-    //     contents: [
-    //       {
-    //         parts: [{ text: prompt }],
-    //         role: "user",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       "x-goog-api-key": GEMINI_API_KEY,
-    //     },
-    //   }
-    // );
+    console.log("Gemini key is ", GEMINI_API_KEY);
+    const res = await axios.post(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
+      {
+        contents: [
+          {
+            parts: [{ text: prompt }],
+            role: "user",
+          },
+        ],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "x-goog-api-key": GEMINI_API_KEY,
+        },
+      }
+    );
 
-    // return res.data.candidates?.[0]?.content?.parts?.[0]?.text ?? "No summary found.";
-    return "We're saving credits";
+    if (res.data.error) {
+      console.error("Gemini summary error:", res.data.error);
+      return "Failed to generate summary.";
+    }
+
+    return res.data.candidates?.[0]?.content?.parts?.[0]?.text ?? "No summary found.";
   } catch (err) {
     console.error("Gemini summary error:", err);
     return "Failed to generate summary.";
